e240baca8cceca7db25f838f1627c752989e070c
RevScoreNet <revscore.net@gmail.com>
2023-10-10 20:54:58 +0300

    class

---
+++ fc.js
@@ -0,0 +1,13 @@
+'use stricit';
+
+export class fc {
+  constructor(obj) {
+    this.obj = obj;
+    //const self = this;
+    this.str = this.str;
+  }
+
+  str(v) {
+    return JSON.stringify(v);
+  }
+}
--- test.js
+++ test.js
@@ -1,1 +1,5 @@
-alert("test");
+import {fc} from './fc.js'
+
+const init = new fc({a: 'b'})
+//console.log(init);
+alert(init.str({test: "abcd"}))
